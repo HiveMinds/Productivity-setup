5442f62197b14c0ac3cf6a4ecef5487c0ca9b56f
a-t-0 <you@example.com>
2022-11-27 00:45:51 +0100

    Included some addons.

--- firefox/boiler_plate.py
+++
@@ -1,8 +0,0 @@
-"""Restores backup with custom filter settings to Ublock."""
-
-website_controller = Website_controller()
-website_controller.driver = open_url(website_controller.driver, login_url)
-website_controller.driver.implicitly_wait(6)
-username_input = website_controller.driver.find_element(
-    "id", user_element_id
-)
